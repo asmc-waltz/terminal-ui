2f4871cf14f4275f3ede1d4743bdbef7c6b602c4
Phuong Nguyen <phuongnguyen0793@outlook.com>
2025-04-21 17:52:15 +0700

    background: generate a gradient bg instance without initializing the handler
    Signed-off-by: Phuong Nguyen <phuongnguyen0793@outlook.com>

--- src/gmisc/gmisc.c
+++ src/gmisc/gmisc.c
@@ -47,6 +47,16 @@ lv_obj_t * g_create_panel(lv_obj_t *parent, lv_style_t *style, int32_t w, int32_
 }
 
 lv_obj_t * g_create_background(lv_obj_t *parent, lv_style_t *style, int32_t w, int32_t h) {
-    return g_create_panel(parent, style, w, h);
+    lv_obj_t *bg = g_create_panel(parent, style, w, h);
+
+    static lv_style_t style_grad;
+    lv_style_init(&style_grad);
+    lv_style_set_bg_color(&style_grad, lv_color_hex(0x478DDF));
+    lv_style_set_bg_grad_color(&style_grad, lv_color_hex(0x66D569));
+    lv_style_set_bg_grad_dir(&style_grad, LV_GRAD_DIR_HOR);
+    lv_style_set_bg_opa(&style_grad, LV_OPA_COVER);
+    lv_obj_add_style(bg, &style_grad, 0);
+
+    return bg;
 }
 
