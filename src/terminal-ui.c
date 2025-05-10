ec242b972a1d88a228d6470345da04bbaef789d5
Phuong Nguyen <phuongnguyen0793@outlook.com>
2025-05-10 17:11:53 +0700

    terminal-ui: move status bar and home indicator to layer sys
    Signed-off-by: Phuong Nguyen <phuongnguyen0793@outlook.com>

--- src/terminal-ui.c
+++ src/terminal-ui.c
@@ -35,9 +35,9 @@ static void sf_create_common_components(void)
 {
     gf_create_background(lv_layer_bottom(), 1024, 600);
     gf_create_setting_menu(lv_screen_active());
-    gf_create_status_bar(lv_layer_top());
+    gf_create_status_bar(lv_layer_sys());
     gf_create_taskbar(lv_layer_top());
-    gf_create_home_indicator(lv_layer_top());
+    gf_create_home_indicator(lv_layer_sys());
     // Initialize the default keyboard that will always be accessible on the top layer.
     gf_keyboard_create();
 }
