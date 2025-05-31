8fd69a24a0df72b01bcfc2fc2eea83d1ebaf6511
Phuong Nguyen <phuongnguyen0793@outlook.com>
2025-05-31 21:34:55 +0700

    terminal-ui: fix build error caused by missing header
    Signed-off-by: Phuong Nguyen <phuongnguyen0793@outlook.com>

--- include/ui_comm.h
+++ include/ui_comm.h
@@ -8,13 +8,21 @@
 /*********************
  *      INCLUDES
  *********************/
-#include <stdio.h>
-#include <stdbool.h>
-#include <lvgl.h>
+#include <stdint.h>
+#include <dbus/dbus.h>
 
 /*********************
  *      DEFINES
  *********************/
+#define SYS_MGR_DBUS_SER                "com.SystemManager.Service"
+#define SYS_MGR_DBUS_OBJ_PATH           "/com/SystemManager/Obj/SysCmd"
+#define SYS_MGR_DBUS_IFACE              "com.SystemManager.Interface"
+#define SYS_MGR_DBUS_METH               "SysMeth"
+
+#define UI_DBUS_SER                     "com.TerminalUI.Service"
+#define UI_DBUS_OBJ_PATH                "/com/TerminalUI/Obj/UsrCmd"
+#define UI_DBUS_IFACE                   "com.TerminalUI.Interface"
+#define UI_DBUS_SIG                     "UISig"
 
 /**********************
  *      TYPEDEFS
--- src/comm/proc_comm.c
+++ src/comm/proc_comm.c
@@ -7,6 +7,7 @@
  *      INCLUDES
  *********************/
 #include <stdio.h>
+#include <stdbool.h>
 
 #include <ui_comm.h>
 #include <log.h>
