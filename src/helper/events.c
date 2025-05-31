70cf82bbfdcba6ae083f6c7f35eeb145027dc186
Phuong Nguyen <phuongnguyen0793@outlook.com>
2025-05-31 21:39:15 +0700

    events: clone handler from sys-mgr for signal broadcasting
    Signed-off-by: Phuong Nguyen <phuongnguyen0793@outlook.com>

---
+++ src/helper/events.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/eventfd.h>
+
+#include <log.h>
+#include <ui_comm.h>
+
+int event_fd;
+
+void event_set(int evfd, uint64_t code)
+{
+    uint64_t val = code;
+    write(evfd, &val, sizeof(val));
+}
+
+uint64_t event_get(int evfd)
+{
+    uint64_t val;
+    read(evfd, &val, sizeof(uint64_t));
+    return val;
+}
+
+int init_event_file()
+{
+    event_fd = eventfd(0, EFD_NONBLOCK);
+    if (event_fd == -1) {
+        return -1;
+    }
+    return 0;
+}
+
