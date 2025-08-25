523a126cefa5a500042e86bffb22ae9db987fe40
Phuong Nguyen <phuongnguyen0793@outlook.com>
2025-08-25 13:44:48 +0700

    ui: styles: support local log level
    Signed-off-by: Phuong Nguyen <phuongnguyen0793@outlook.com>

--- src/ui/styles/styles.c
+++ src/ui/styles/styles.c
@@ -6,12 +6,17 @@
 /*********************
  *      INCLUDES
  *********************/
+// #define LOG_LEVEL LOG_LEVEL_TRACE
+#if defined(LOG_LEVEL)
+#warning "LOG_LEVEL=" TOSTRING(LOG_LEVEL) ", will take precedence in this file."
+#endif
+#include <log.h>
+
 #include <stdint.h>
 
 #include <lvgl.h>
 #include <style.h>
 #include <fonts.h>
-#include <log.h>
 #include <list.h>
 
 /*********************
