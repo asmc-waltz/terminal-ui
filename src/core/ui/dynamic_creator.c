ca5e942b9036574b066792fe65bf1f06e0331cdb
Phuong Nguyen <phuongnguyen0793@outlook.com>
2025-09-01 14:21:52 +0700

    ui: rotate: update dynamic creator based on global rotation var
    Signed-off-by: Phuong Nguyen <phuongnguyen0793@outlook.com>

--- src/core/ui/dynamic_creator.c
+++ src/core/ui/dynamic_creator.c
@@ -57,61 +57,62 @@ static int32_t g_scr_rot_dir = ROT_0;
  **********************/
 // NOTE
 // Location must be calculate after new size is calculated
-static int32_t g_get_new_xy_ofs(g_obj *pg_obj, int32_t new_rot, int32_t par_w, int32_t par_h)
+static int32_t g_obj_get_new_xy_ofs(g_obj *pg_obj, int32_t par_w, int32_t par_h)
 {
     int32_t real_x_ofs = 0;
     int32_t real_y_ofs = 0;
+    int32_t scr_rot = g_get_scr_rot_dir();
 
     if (!pg_obj) {
         LOG_ERROR("Invalid g object");
         return -EINVAL;
     }
 
-    if (new_rot == pg_obj->inf.rot) {
+    if (scr_rot == pg_obj->inf.rot) {
         return 0;
     }
 
     if (pg_obj->inf.rot == ROT_0) {
-        if (new_rot == ROT_90) {
+        if (scr_rot == ROT_90) {
             real_x_ofs = par_w - (pg_obj->inf.w + pg_obj->inf.y_ofs);
             real_y_ofs = pg_obj->inf.x_ofs;
-        } else if (new_rot == ROT_180) {
+        } else if (scr_rot == ROT_180) {
             real_x_ofs = par_w - (pg_obj->inf.x_ofs + pg_obj->inf.w);
             real_y_ofs = par_h - (pg_obj->inf.y_ofs + pg_obj->inf.h);
-        } else if (new_rot == ROT_270) {
+        } else if (scr_rot == ROT_270) {
             real_x_ofs = pg_obj->inf.y_ofs;
             real_y_ofs = par_h - (pg_obj->inf.h + pg_obj->inf.x_ofs);
         }
     } else if (pg_obj->inf.rot == ROT_90) {
-        if (new_rot == ROT_0) {
+        if (scr_rot == ROT_0) {
             real_x_ofs = pg_obj->inf.y_ofs;
             real_y_ofs = par_w - (pg_obj->inf.x_ofs + pg_obj->inf.h);
-        } else if (new_rot == ROT_180) {
+        } else if (scr_rot == ROT_180) {
             real_x_ofs = par_w - (pg_obj->inf.y_ofs + pg_obj->inf.w);
             real_y_ofs = par_h - (par_w - pg_obj->inf.x_ofs);
-        } else if (new_rot == ROT_270) {
+        } else if (scr_rot == ROT_270) {
             real_x_ofs = par_w - (pg_obj->inf.x_ofs + pg_obj->inf.w);
             real_y_ofs = par_h - (pg_obj->inf.y_ofs + pg_obj->inf.h);
         }
     } else if (pg_obj->inf.rot == ROT_180) {
-        if (new_rot == ROT_0) {
+        if (scr_rot == ROT_0) {
             real_x_ofs = par_w - (pg_obj->inf.x_ofs + pg_obj->inf.w);
             real_y_ofs = par_h - (pg_obj->inf.y_ofs + pg_obj->inf.h);
-        } else if (new_rot == ROT_90) {
+        } else if (scr_rot == ROT_90) {
             real_x_ofs = par_w - (par_h - pg_obj->inf.y_ofs);
             real_y_ofs = par_w - (pg_obj->inf.x_ofs + pg_obj->inf.h);
-        } else if (new_rot == ROT_270) {
+        } else if (scr_rot == ROT_270) {
             real_x_ofs = par_h - (pg_obj->inf.y_ofs + pg_obj->inf.w);
             real_y_ofs = par_h - (par_w - pg_obj->inf.x_ofs);
         }
     } else if (pg_obj->inf.rot == ROT_270) {
-        if (new_rot == ROT_0) {
+        if (scr_rot == ROT_0) {
             real_x_ofs = par_h - (pg_obj->inf.y_ofs + pg_obj->inf.w);
             real_y_ofs = pg_obj->inf.x_ofs;
-        } else if (new_rot == ROT_90) {
+        } else if (scr_rot == ROT_90) {
             real_x_ofs = par_w - (pg_obj->inf.x_ofs + pg_obj->inf.w);
             real_y_ofs = par_h - (pg_obj->inf.y_ofs + pg_obj->inf.h);
-        } else if (new_rot == ROT_180) {
+        } else if (scr_rot == ROT_180) {
             real_x_ofs = par_w - (par_h - pg_obj->inf.y_ofs);
             real_y_ofs = par_h - (pg_obj->inf.x_ofs + pg_obj->inf.h);
         }
@@ -131,27 +132,35 @@ static int32_t g_get_new_xy_ofs(g_obj *pg_obj, int32_t new_rot, int32_t par_w, i
         return -1;
     }
 
-    LOG_INFO("ROTATE %d, X=%d - Y=%d", new_rot, real_x_ofs, real_y_ofs);
+    LOG_INFO("ROTATE %d, X=%d - Y=%d", scr_rot, real_x_ofs, real_y_ofs);
 
     return 0;
 }
 
 
-static int32_t g_obj_relocation(g_obj *pg_obj, int32_t rot)
+static int32_t g_obj_rot_relocation(g_obj *pg_obj)
 {
+    lv_obj_t *pl_obj_par = NULL;
+    g_obj *pg_obj_par = NULL;
+
     if (!pg_obj) {
         LOG_ERROR("Invalid g object");
         return -EINVAL;
     }
 
-    lv_obj_t *pl_par = NULL;
-    pl_par = lv_obj_get_parent(pg_obj->obj);
+    pl_obj_par = lv_obj_get_parent(pg_obj->obj);
+    if (!pl_obj_par) {
+        LOG_ERROR("Invalid lvgl object");
+        return -EINVAL;
+    }
 
-    g_obj *pg_obj_par = NULL;
-    pg_obj_par = pl_par->user_data;
+    pg_obj_par = pl_obj_par->user_data;
+    if (!pg_obj_par) {
+        LOG_ERROR("Invalid g parent object");
+        return -EINVAL;
+    }
 
-
-    g_get_new_xy_ofs(pg_obj, rot, pg_obj_par->inf.w, pg_obj_par->inf.h);
+    g_obj_get_new_xy_ofs(pg_obj, pg_obj_par->inf.w, pg_obj_par->inf.h);
 
     lv_obj_set_pos(pg_obj->obj, pg_obj->inf.x_ofs, pg_obj->inf.y_ofs);
     return 0;
@@ -166,8 +175,10 @@ static void g_swap_xy_size(g_obj *pg_obj)
     pg_obj->inf.h = tmp_width;
 }
 
-static int32_t g_obj_resize(g_obj *pg_obj, int32_t rot)
+static int32_t g_obj_rot_resize(g_obj *pg_obj)
 {
+    int32_t scr_rot = g_get_scr_rot_dir();
+
     if (!pg_obj) {
         LOG_ERROR("Invalid g object");
         return -EINVAL;
@@ -175,16 +186,16 @@ static int32_t g_obj_resize(g_obj *pg_obj, int32_t rot)
 
     // Must check: input rot is different from the current rotation inf
 
-    if (rot == ROT_0) {
+    if (scr_rot == ROT_0) {
         if (pg_obj->inf.rot == ROT_90 || pg_obj->inf.rot == ROT_270)
             g_swap_xy_size(pg_obj);
-    } else if (rot == ROT_90) {
+    } else if (scr_rot == ROT_90) {
         if (pg_obj->inf.rot == ROT_0 || pg_obj->inf.rot == ROT_180)
             g_swap_xy_size(pg_obj);
-    } else if (rot == ROT_180) {
+    } else if (scr_rot == ROT_180) {
         if (pg_obj->inf.rot == ROT_90 || pg_obj->inf.rot == ROT_270)
             g_swap_xy_size(pg_obj);
-    } else if (rot == ROT_270) {
+    } else if (scr_rot == ROT_270) {
         if (pg_obj->inf.rot == ROT_0 || pg_obj->inf.rot == ROT_180)
             g_swap_xy_size(pg_obj);
     }
@@ -194,31 +205,40 @@ static int32_t g_obj_resize(g_obj *pg_obj, int32_t rot)
 }
 
 
-static int32_t g_obj_rotate_relocation(g_obj *pg_obj, int32_t rot_dir)
+static int32_t g_obj_rotate(g_obj *pg_obj)
 {
     int32_t ret;
-    int32_t tmp_val;
+    int32_t scr_rot = g_get_scr_rot_dir();
 
     if (!pg_obj) {
         LOG_ERROR("Invalid g object");
         return -EINVAL;
     }
 
-    if (pg_obj->inf.rot == rot_dir) {
+    if (pg_obj->inf.rot == scr_rot) {
         LOG_TRACE("Object is rotated already");
         return 0;
     } else {
-        LOG_TRACE("Object is rotating to %d", rot_dir);
+        LOG_TRACE("Object is rotating to %d", scr_rot);
     }
 
+    // TODO: check obj type and update flex flow, scale...
     // Text, icon, switch will be rotate
     // Frame, button, slider will be resize and relocation
 
-    // TODO: check return and obj type
-    g_obj_resize(pg_obj, rot_dir);
-    g_obj_relocation(pg_obj, rot_dir);
+    ret = g_obj_rot_resize(pg_obj);
+    if (ret) {
+        return -EINVAL;
+    }
 
-    pg_obj->inf.rot = rot_dir;
+    ret= g_obj_rot_relocation(pg_obj);
+    if (ret) {
+        return -EINVAL;
+    }
+
+    pg_obj->inf.rot = scr_rot;
+
+    return 0;
 }
 
 /**********************
