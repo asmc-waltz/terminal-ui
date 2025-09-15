feb6bd333a296e92712d757d0db76bdacbf80785
Phuong Nguyen <phuongnguyen0793@outlook.com>
2025-09-15 13:01:00 +0700

    ui: testing: add the common screen btn for testing purpose
    Signed-off-by: Phuong Nguyen <phuongnguyen0793@outlook.com>

--- src/ui/pages/common.c
+++ src/ui/pages/common.c
@@ -28,6 +28,7 @@
 /*********************
  *      DEFINES
  *********************/
+#define SCREEN_BG_COLOR                 0x292D33
 
 /**********************
  *      TYPEDEFS
@@ -56,6 +57,40 @@
 /**********************
  *   GLOBAL FUNCTIONS
  **********************/
+#define TEST 1
+#if defined(TEST)
+#include <time.h>
+
+int32_t get_random_3(void)
+{
+    int32_t val;
+
+    val = rand() % 4;
+    return val;
+}
+
+static void rotate_key_handler(lv_event_t *event)
+{
+    lv_obj_t *lobj = gf_get_obj_by_name("screens.common", \
+                            &((g_obj *)(lv_screen_active()->user_data))->child);
+
+    g_set_scr_rot_dir(get_random_3());
+    refresh_obj_tree_layout(lobj->user_data);
+}
+
+static void create_keyboard_handler(lv_event_t *event)
+{
+    lv_obj_t *comm_page;
+    comm_page = gf_get_obj_by_name("screens.common", &get_gobj(lv_screen_active())->child);
+    if (!comm_page) {
+        LOG_ERROR("Screen [%s] not found", "screens.common");
+    }
+
+    create_keyboard(comm_page);
+}
+
+#endif
+
 lv_obj_t *create_scr_page(lv_obj_t *par, const char *name)
 {
     int32_t obj_w, obj_h;
@@ -67,13 +102,36 @@ lv_obj_t *create_scr_page(lv_obj_t *par, const char *name)
     gf_gobj_set_pos(page, 0, 0);
 
     // TODO: create background
-    lv_obj_set_style_bg_color(page, lv_color_hex(0x000000), 0);
+    lv_obj_set_style_bg_color(page, lv_color_hex(SCREEN_BG_COLOR), 0);
 
 
-    lv_obj_t *top_bar = create_top_bar(page);
-    lv_obj_t *sym_box = add_top_bar_symbol(top_bar, "comps.top_bar.wifi",
+    lv_obj_t *top_space = create_top_bar(page);
+    lv_obj_t *sym_box = add_top_bar_symbol(top_space, TOP_BAR_NAME".wifi",
                                                 ICON_WIFI_SOLID);
-    gf_gobj_align_to(sym_box, top_bar, LV_ALIGN_TOP_LEFT, TOP_BAR_SYM_ALN, 0);
+    gf_gobj_align_to(sym_box, top_space, LV_ALIGN_TOP_LEFT, TOP_BAR_SYM_ALN, 0);
+
+    // TODO: debug setting page
+    create_setting_page(page);
+
+
+#if defined(TEST)
+    lv_obj_t *btn = gf_create_btn(lv_layer_top(), "common.rotate_btn");
+    lv_obj_add_event_cb(btn, rotate_key_handler, LV_EVENT_CLICKED, btn->user_data);
+    gf_gobj_set_size(btn, 54, 54);
+    gf_gobj_align_to(btn, lv_layer_top(), LV_ALIGN_CENTER, -50, 0);
+
+    lv_obj_t *icon = gf_create_sym(btn, NULL, 10, 10, \
+                         TOP_BAR_SYM_FONTS, ICON_ROTATE_SOLID, lv_color_hex(0xFFFFFF));
+
+
+    btn = gf_create_btn(lv_layer_top(), "create keyboard");
+    gf_gobj_set_size(btn, 54, 54);
+    gf_gobj_align_to(btn, lv_layer_top(), LV_ALIGN_CENTER, 50, 0);
+    lv_obj_add_event_cb(btn, create_keyboard_handler, LV_EVENT_CLICKED, btn->user_data);
+
+    icon = gf_create_sym(btn, NULL, 10, 10, \
+                         TOP_BAR_SYM_FONTS, ICON_KEYBOARD, lv_color_hex(0xFFFFFF));
+#endif
 
     return page;
 }
