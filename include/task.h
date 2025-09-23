1ba799d5288e44e968b9d3845c6e37f85474041b
Phuong Nguyen <phuongnguyen0793@outlook.com>
2025-09-23 17:57:26 +0700

    main: add application runtime data structures
    Signed-off-by: Phuong Nguyen <phuongnguyen0793@outlook.com>

--- include/task.h
+++ include/task.h
@@ -10,7 +10,11 @@
  *********************/
 #include <stdint.h>
 #include <stdbool.h>
+#include <signal.h>
+#include <dbus/dbus.h>
 
+#include <lvgl.h>
+#include "list.h"
 #include "sched/workqueue.h"
 
 /*********************
@@ -20,6 +24,42 @@
 /**********************
  *      TYPEDEFS
  **********************/
+typedef struct screen {
+    lv_display_t *drm_disp;
+    lv_indev_t *touch_event;
+    int32_t width;
+    int32_t height;
+    int32_t rotation;
+} screen_t;
+
+typedef struct ui_obj {
+    struct list_head obj_lst;           /* List of registered UI objects */
+    uint32_t next_id;
+} obj_t;
+
+typedef struct op_handler {
+    struct list_head handler_lst;       /* List of registered opcode handlers */
+    wq_ctx_t *wq_ctxs;                  /* List of registered workqueues */
+} op_t;
+
+typedef struct comm_handler {
+    DBusConnection *dbus_conn;
+    int32_t event_fd;
+    sig_atomic_t run;
+} comm_t;
+
+typedef struct conf_data {
+    int8_t brightness;
+    int8_t als_en;
+} conf_t;
+
+typedef struct ctx {
+    screen_t scr;
+    obj_t ui;
+    op_t op;
+    comm_t comm;
+    conf_t cfg;
+} ctx_t;
 
 /**********************
  *  GLOBAL VARIABLES
