b6a8b7570675c74cb7644a6e12d71c0076df0ce0
Phuong Nguyen <phuongnguyen0793@outlook.com>
2025-09-27 11:35:56 +0700

    pages: setting: add brightness.c file
    Signed-off-by: Phuong Nguyen <phuongnguyen0793@outlook.com>

---
+++ src/ui/pages/setting/brightness.c
@@ -0,0 +1,58 @@
+/**
+ * @file brightness.c
+ *
+ */
+
+/*********************
+ *      INCLUDES
+ *********************/
+// #define LOG_LEVEL LOG_LEVEL_TRACE
+#if defined(LOG_LEVEL)
+#warning "LOG_LEVEL defined locally will override the global setting in this file"
+#endif
+#include "log.h"
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <errno.h>
+
+#include <lvgl.h>
+#include "list.h"
+#include "ui/ui_core.h"
+#include "ui/fonts.h"
+#include "ui/comps.h"
+#include "ui/pages.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ *  GLOBAL VARIABLES
+ **********************/
+
+/**********************
+ *  STATIC PROTOTYPES
+ **********************/
+
+/**********************
+ *  STATIC VARIABLES
+ **********************/
+
+/**********************
+ *      MACROS
+ **********************/
+
+/**********************
+ *   STATIC FUNCTIONS
+ **********************/
+
+/**********************
+ *   GLOBAL FUNCTIONS
+ **********************/
