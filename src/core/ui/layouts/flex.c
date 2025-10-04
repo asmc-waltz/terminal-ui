c92ae23da4501ba8e6691f9f44faae6bff0f9cc5
Phuong Nguyen <phuongnguyen0793@outlook.com>
2025-10-04 14:43:32 +0700

    ui: core: add flex.h and flex.c to support flex layout
    Signed-off-by: Phuong Nguyen <phuongnguyen0793@outlook.com>

---
+++ src/core/ui/layouts/flex.c
@@ -0,0 +1,55 @@
+/**
+ * @file flex.c
+ *
+ */
+
+/*********************
+ *      INCLUDES
+ *********************/
+#define LOG_LEVEL LOG_LEVEL_TRACE
+#if defined(LOG_LEVEL)
+#warning "LOG_LEVEL defined locally will override the global setting in this file"
+#endif
+#include "log.h"
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <errno.h>
+
+#include <lvgl.h>
+#include "list.h"
+#include "ui/ui_core.h"
+#include "ui/flex.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ *  GLOBAL VARIABLES
+ **********************/
+
+/**********************
+ *  STATIC PROTOTYPES
+ **********************/
+
+/**********************
+ *  STATIC VARIABLES
+ **********************/
+
+/**********************
+ *      MACROS
+ **********************/
+
+/**********************
+ *   STATIC FUNCTIONS
+ **********************/
+
+/**********************
+ *   GLOBAL FUNCTIONS
+ **********************/
+
