bea3de9f1925839092c4253e22ec784b16265edf
Phuong Nguyen <phuongnguyen0793@outlook.com>
2025-10-15 20:54:41 +0700

    ui: header: add widget.h for all widget
    Signed-off-by: Phuong Nguyen <phuongnguyen0793@outlook.com>

---
+++ include/ui/widget.h
@@ -0,0 +1,56 @@
+/**
+ * @file widget.h
+ *
+ */
+
+#ifndef G_WIDGET_H
+#define G_WIDGET_H
+/*********************
+ *      INCLUDES
+ *********************/
+#include <stdint.h>
+
+#include <lvgl.h>
+#include "ui/ui_core.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ *  GLOBAL VARIABLES
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+/*=====================
+ * Setter functions
+ *====================*/
+int32_t set_active_menu_page(lv_obj_t *lobj, \
+                             lv_obj_t *(*create_page_cb)(lv_obj_t *, \
+                                                         const char *));
+
+/*=====================
+ * Getter functions
+ *====================*/
+
+/*=====================
+ * Other functions
+ *====================*/
+lv_obj_t *create_menu(lv_obj_t *par, const char *name);
+lv_obj_t *create_menu_bar(lv_obj_t *menu);
+lv_obj_t *create_menu_group(lv_obj_t *par, const char *name);
+lv_obj_t *create_menu_item(lv_obj_t *menu, lv_obj_t *menu_bar, \
+                           const char *sym_index, const char *title,\
+                           lv_obj_t *(* create_page_cb)(lv_obj_t *, const char *));
+
+/**********************
+ *      MACROS
+ **********************/
+
+#endif /* G_WIDGET_H */
