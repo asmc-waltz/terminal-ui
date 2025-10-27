f4d1cd7e77bb663f5250752c40fb085d83fa4976
Phuong Nguyen <phuongnguyen0793@outlook.com>
2025-10-27 20:35:19 +0700

    ui: setting: wifi: support function show and update access points
    Signed-off-by: Phuong Nguyen <phuongnguyen0793@outlook.com>

--- src/ui/windows/setting/wifi.c
+++ src/ui/windows/setting/wifi.c
@@ -47,6 +47,9 @@
 /**********************
  *  STATIC VARIABLES
  **********************/
+static lv_obj_t *wifi_general_group = NULL;
+static lv_obj_t *wifi_connected_ap = NULL;
+static lv_obj_t *ap_holder = NULL;
 
 /**********************
  *      MACROS
@@ -75,58 +78,199 @@ static void switch_wifi_enable_handler(lv_event_t *e)
                   enable ? "Enable" : "Disable", ret);
 }
 
-static int32_t create_wifi_setting_items(lv_obj_t *par)
+static lv_obj_t *create_wifi_about_section(lv_obj_t *par)
 {
-    lv_obj_t *group, *child_group, *sym, *label, *swit;
+    lv_obj_t *group, *sym, *label;
     const char *desc = "Connect to available wireless networks\n"
-                       "or manage saved connections";
+               "or manage saved connections";
 
-    if (!par)
-        return -EINVAL;
-
-    /* Section: Brightness description */
     group = create_vertical_flex_group(par, "WIFI-ABOUT");
     if (!group)
-        return -ENOMEM;
+        return NULL;
 
-    sym = create_symbol_box(group, NULL, &terminal_icons_48, \
-                            ICON_WIFI_SOLID);
+    sym = create_symbol_box(group, NULL, &terminal_icons_48, ICON_WIFI_SOLID);
     if (!sym)
-        return -EIO;
+        return NULL;
 
     label = create_text_box(group, NULL, &lv_font_montserrat_24, desc);
     if (!label)
-        return -EIO;
+        return NULL;
 
-    /* Airplane mode toggle */
-    group = create_horizontal_flex_group(group, "WIFI-SWITCH");
+    return group;
+}
+
+static int32_t create_wifi_toggle_section(lv_obj_t *par)
+{
+    lv_obj_t *group, *label, *switch_box;
+
+    group = create_horizontal_flex_group(par, "WIFI-SWITCH");
     if (!group)
         return -ENOMEM;
+
+    set_padding(group, 10, 10, 10, 10);
+
+    label = create_text_box(group, NULL, &lv_font_montserrat_24, "Wi-Fi");
+    if (!label)
+        return -EIO;
+
+    switch_box = create_switch_box(group, NULL);
+    if (!switch_box)
+        return -EIO;
+
+    lv_obj_add_event_cb(get_box_child(switch_box), \
+                switch_wifi_enable_handler, \
+                LV_EVENT_ALL, NULL);
+
+    return 0;
+}
+
+static lv_obj_t *create_wifi_ap_item(lv_obj_t *parent, \
+                     const char *ssid, \
+                     int8_t strength)
+{
+    lv_obj_t *group, *label;
+    char str_buf[10];
+
+    group = create_horizontal_flex_group(parent, ssid);
+    if (!group)
+        return NULL;
+
+    set_padding(group, 10, 10, 10, 10);
+
+    label = create_text_box(group, NULL, &lv_font_montserrat_24, ssid);
+    if (!label)
+        return NULL;
+
+    snprintf(str_buf, sizeof(str_buf), "%d%%", strength);
+    label = create_text_box(group, NULL, &lv_font_montserrat_24, str_buf);
+    if (!label)
+        return NULL;
+
+    return group;
+}
+
+/*
+ *  Update or add current connected Wi-Fi AP
+ */
+static int32_t add_wifi_connected_ap(const char *ssid, int8_t strength)
+{
+    lv_obj_t *group;
+    const char *old_name = NULL;
+
+    if (!ssid)
+        return -EINVAL;
+
+    if (!lv_obj_is_valid(wifi_general_group))
+        return -EIO;
+
+    /* Remove previous connected AP if exists */
+    if (lv_obj_is_valid(wifi_connected_ap)) {
+        old_name = get_name(wifi_connected_ap);
+
+        LOG_INFO("Wi-Fi connected AP changed [%s] -> [%s - %d%%]", \
+             old_name ? old_name : "(unknown)", ssid, strength);
+
+        remove_obj_and_child_by_name(old_name, \
+                         &get_meta(wifi_general_group)->child);
+
+        wifi_connected_ap = NULL;
+    }
+
+    group = create_wifi_ap_item(wifi_general_group, ssid, strength);
+    if (!group)
+        return -ENOMEM;
+
+    wifi_connected_ap = group;
 
     set_border_side(group, LV_BORDER_SIDE_TOP);
     lv_obj_set_style_border_width(group, 2, 0);
     lv_obj_set_style_border_color(group, lv_color_black(), 0);
 
-    label = create_text_box(group, NULL, \
-                            &lv_font_montserrat_24, "Wi-Fi");
-    if (!label)
+    LOG_DEBUG("Wi-Fi connected AP added: %s (%d%%)", ssid, strength);
+    return 0;
+}
+
+static int32_t add_available_wifi_ap(const char *ssid, int8_t strength)
+{
+    lv_obj_t *group;
+
+    if (!ssid)
+        return -EINVAL;
+
+    if (!lv_obj_is_valid(ap_holder))
         return -EIO;
 
-    swit = create_switch_box(group, NULL);
-    if (!swit)
-        return -EIO;
-    lv_obj_add_event_cb(get_box_child(swit), \
-                        switch_wifi_enable_handler, \
-                        LV_EVENT_ALL, NULL);
+    group = create_wifi_ap_item(ap_holder, ssid, strength);
+    if (!group)
+        return -ENOMEM;
 
-    /* Section: Spacer (flex filler) */
-    lv_obj_t *filler = create_box(par, "FILLER");
+    /* Add border only if not the first AP */
+    if (lv_obj_get_child(ap_holder, 0) != group) {
+        set_border_side(group, LV_BORDER_SIDE_TOP);
+        lv_obj_set_style_border_width(group, 2, 0);
+        lv_obj_set_style_border_color(group, lv_color_black(), 0);
+    }
+
+    return 0;
+}
+
+static int32_t create_wifi_filler(lv_obj_t *par)
+{
+    lv_obj_t *filler;
+
+    filler = create_box(par, "FILLER");
     if (!filler)
         return -EIO;
 
-    set_size(filler, LV_PCT(100), LV_PCT(100));
+    set_size(filler, LV_PCT(100), LV_PCT(70));
+    return 0;
+}
 
-    LOG_DEBUG("Wifi setting items created");
+/*---------------------------------------------*
+ *  Main Wi-Fi settings item builder
+ *---------------------------------------------*/
+static int32_t create_wifi_setting_items(lv_obj_t *par)
+{
+    lv_obj_t *about_group, *general_group;
+    int32_t ret;
+
+    if (!par)
+        return -EINVAL;
+
+    /* Section: About */
+    about_group = create_wifi_about_section(par);
+    if (!about_group)
+        return -EIO;
+
+    /* Section: General */
+    general_group = create_vertical_flex_group(about_group, "WIFI-GENERAL");
+    if (!general_group)
+        return -ENOMEM;
+
+    wifi_general_group = general_group;
+
+    set_padding(general_group, 0, 0, 20, 0);
+    set_row_padding(general_group, 0);
+    set_border_side(general_group, LV_BORDER_SIDE_TOP);
+    lv_obj_set_style_border_width(general_group, 2, 0);
+    lv_obj_set_style_border_color(general_group, lv_color_black(), 0);
+
+    ret = create_wifi_toggle_section(general_group);
+    if (ret < 0)
+        return ret;
+
+    ap_holder = create_vertical_flex_group(par, "WIFI-AP-HOLDER");
+    if (!ap_holder)
+        return -ENOMEM;
+
+    set_padding(ap_holder, 0, 0, 40, 20);
+    set_row_padding(ap_holder, 0);
+
+    ret = create_wifi_filler(par);
+    if (ret < 0)
+        return ret;
+
+    LOG_DEBUG("Wi-Fi setting items created successfully");
     return 0;
 }
 
